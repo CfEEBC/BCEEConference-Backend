a29a92d95a588f5614b65eca770bd1f337152666
Daniel Gipps <dgipps@dhcp-128-189-226-38.ubcsecure.wireless.ubc.ca>
2013-10-25 22:11:56 -0700

    Addes session.py

--- main.py
+++ main.py
@@ -15,11 +15,44 @@
 # limitations under the License.
 #
 import webapp2
+import session
+
+
+MAIN_PAGE_FOOTER_TEMPLATE = """\
+    <form action="/" method="post">
+      Session Location
+      <div><textarea name="session_location" rows="3" cols="60"></textarea></div>
+      Session Description
+      <div><textarea name="session_description" rows="3" cols="60"></textarea></div>
+      Session Name
+      <div><textarea name="session_name" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Submit Information"></div>
+    </form>
+
+    
+
+   
+
+  </body>
+</html>
+"""
 
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE)
+
+    def post(self):
+    	session1 = Session(name=self.request.get("session_name")
+    		, description=self.request.get("session_description")
+    		, location=self.request.get("session_location"))
+
+    	session1.put()	 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+
+
+		
